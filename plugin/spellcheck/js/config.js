c0d8ecd4a95f9784cf2c09fbb4cc8eb189a47be7
Gradler <sungjoong.kim@lotte.net>
2020-09-15 13:04:49 +0900

    initial commit

---
+++ plugin/spellcheck/js/config.js
@@ -0,0 +1,4 @@
+﻿// 맞춤법 검사 핸들러 URL
+G_DEPlugin["spellcheck"].handlerURL = "http://dict.raonwiz.com:8080/spellchecker/spellcheck_handler.jsp";
+// 맞춤법 검사 약한규칙(true), 강한규칙(false)
+G_DEPlugin["spellcheck"].weakRule = false;
